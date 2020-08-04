f2dd334b9dc215235d1f186d9d5cddde9997209c
saihaj <44710980+saihaj@users.noreply.github.com>
2020-08-03 21:38:44 -0500

    remove old stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,29 +7,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'mdx',
-        path: `${__dirname}/content`,
-      },
-    },
-    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Saihajpreet Singh',
         short_name: 'saihaj',
         start_url: '/',
       },
-    },
-    {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        extensions: [ '.mdx', '.md' ],
-        defaultLayouts: {
-          default: require.resolve( './src/components/Layout.js' ),
-        },
-      },
-    },
-    'gatsby-plugin-styled-components',
+    }
   ],
 }
