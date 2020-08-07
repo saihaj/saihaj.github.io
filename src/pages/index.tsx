bfd48feb08ed2ad977d2195dddb0665b1e9c8988
saihaj <44710980+saihaj@users.noreply.github.com>
2020-08-06 20:30:48 -0500

    feat: add homepage hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,13 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import { createUseStyles } from 'react-jss'
 
-import SEO from '../components/Seo'
+import Seo from '../components/Seo'
+import Hero from '../components/Hero'
 
-const useStyles = createUseStyles( {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  title: {
-    color: 'white',
-    fontFamily: 'Rubik',
-    fontSize: '4.7rem',
-    paddingTop: '15vh',
-    fontWeight: '900',
-  },
-  memoji: {
-    width: '300px',
-  },
-} )
-
-const IndexPage:React.FC = () => {
-  const { file: { childImageSharp } } = useStaticQuery( graphql`
-  query FaviconImage {
-    file( sourceInstanceName: { eq: "images" }, name: { eq: "favicon" } ) {
-      childImageSharp {
-        fluid(quality:100, maxWidth:800) {
-          ...GatsbyImageSharpFluid_withWebp_tracedSVG
-        }
-      }
-    }
-  }
-` )
-  const styles = useStyles()
-
-  return (
-    <div className={styles.container}>
-      <SEO title="Home" />
-      <h1 className={styles.title}>Coming Soon!</h1>
-      <Img className={styles.memoji} alt="Saihajpreet Singh's Memoji" fluid={childImageSharp.fluid} />
-    </div>
-  )
-}
+const IndexPage:React.FC = () => (
+  <div>
+    <Seo title="Home" />
+    <Hero />
+  </div>
+)
 
 export default IndexPage
