950eea3197ed7bdb7495283356b40d26ccdcd4f6
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-21 16:03:43 -0600

    feat: remove react typical

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,6 @@
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import Typical from 'react-typical'
 
-const intro = [
-  'Hey there 👋 !', 1000,
-  'Thanks for stopping by ', 1000,
-  'I am a student 🎓', 1000,
-  'Majoring Computer Science 💻', 1000,
-  'Scroll down to learn more about me 👇', 1000,
-]
 
 const Hero = () => {
   const { file: { childImageSharp } } = useStaticQuery( graphql`
@@ -28,7 +20,6 @@ const Hero = () => {
       <div className="-mt-24">
         <Img alt="Saihajpreet Singh's Memoji" className="mx-auto w-64" fluid={childImageSharp.fluid} />
         <h1 className="font-fugaz text-6xl text-center">Saihajpreet Singh</h1>
-        <Typical className="text-center text-3xl" wrapper="h3" steps={intro} loop={Infinity} />
       </div>
     </div>
   )
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,3 @@
-declare module 'react-typical';
 declare module '*.svg' {
     import React = require( 'react' );
 
