a704e612a22bde27c5ca28c854ee5befc2b63553
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-21 21:30:23 -0600

    feat: add profile picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import SocialIcons from './SocialIcons'
 const Hero = () => {
   const { file: { childImageSharp } } = useStaticQuery( graphql`
   query FaviconImage {
-    file( sourceInstanceName: { eq: "images" }, name: { eq: "favicon" } ) {
+    file( sourceInstanceName: { eq: "images" }, name: { eq: "me" } ) {
       childImageSharp {
         fluid(quality:100, maxWidth:800) {
           ...GatsbyImageSharpFluid_withWebp_tracedSVG
@@ -19,9 +19,19 @@ const Hero = () => {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="-mt-24">
-        <Img alt="Saihajpreet Singh's Memoji" className="mx-auto w-64" fluid={childImageSharp.fluid} />
+
+        <div className="flex">
+          <div className="mx-auto">
+            <div className="border-theme-secondary border-8 rounded-full inline-block">
+              <Img alt="Saihajpreet Singh's Memoji" className="w-48 rounded-full" fluid={childImageSharp.fluid} />
+            </div>
+          </div>
+        </div>
+
         <h1 className="font-fugaz text-6xl text-center">Saihajpreet Singh</h1>
+
         <SocialIcons />
+
       </div>
     </div>
   )
