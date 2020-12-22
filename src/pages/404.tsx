71869b543b0ac7dbf3bc41d2a22bc88d2b292006
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-21 21:30:30 -0600

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import SEO from '../components/Seo'
+
+const NotFound = () => (
+  <div className="flex h-screen items-center justify-center">
+    <SEO title="Not Found" />
+    <h1 className="-mt-48 text-5xl font-semibold text-center">
+      Page Not Found
+    </h1>
+  </div>
+)
+
+export default NotFound
