9671efddbde304a63d2192c066c0612d23fcdc9d
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-01-05 21:37:17 -0600

    feat: pass in classname and spread div props

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,13 @@
-import { ReactNode } from 'react'
+import { ReactNode, DetailedHTMLProps, HTMLAttributes } from 'react'
+import cx from 'classnames'
 
 type ContainerProps = {
-    children: ReactNode
-}
+  children: ReactNode
+  className?: string
+} & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
-const Container = ( { children }:ContainerProps ) => (
-  <div className="container mx-auto max-w-4xl px-2 md:px-0">
+const Container = ( { children, className, ...props }:ContainerProps ) => (
+  <div className={cx( 'container mx-auto max-w-4xl px-2 md:px-0', className )} {...props}>
     {children}
   </div>
 )
