49d28990ea914b6a84a4d25f7e8439e483d02521
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-01-07 22:11:55 -0600

    refactor: svg icon component

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,16 +1,5 @@
-import { AnchorHTMLAttributes } from 'react'
 import SOCIAL_PROFILES from '../content/SocialProfiles'
-
-type IconProps ={
-  url: AnchorHTMLAttributes<HTMLAnchorElement>['href']
-  svg: string // see types.d.ts
-}
-
-const Icon = ( { url, svg: Svg }:IconProps ) => (
-  <a aria-label={url} href={url} target="_blank" rel="noreferrer" className="fill-current text-white hover:text-gray-400">
-    <Svg />
-  </a>
-)
+import Icon from './SvgIcon'
 
 const SocialIcons = () => (
   <div className="flex mt-8 justify-center space-x-3">
