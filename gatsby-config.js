8415b415b98da11524714d16f0894361d347afb8
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-03-11 12:40:48 -0600

    fix: add gatsby-plugin-image to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
         path: `${__dirname}/src/content`,
       },
     },
+    'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-transformer-remark',
