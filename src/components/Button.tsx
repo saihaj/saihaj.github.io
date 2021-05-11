0cdf4dc50740d9eb67a23d13656fdea500bc6136
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-05-11 01:14:29 -0400

    feat: navbar and layout

---
+++ src/components/Button.tsx
@@ -0,0 +1,37 @@
+import { DetailedHTMLProps, ButtonHTMLAttributes, ReactNode } from 'react'
+import { createUseStyles } from 'react-jss'
+import cx from 'classnames'
+
+import { Colors } from '../theme'
+
+const useStyles = createUseStyles( {
+  btn: {
+    margin: 0,
+    padding: 0,
+    cursor: 'pointer',
+    backgroundColor: 'transparent',
+    color: Colors.white,
+    borderColor: Colors.yellow,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    minWidth: '8rem',
+    '&:hover': {
+      backgroundColor: Colors.yellow,
+      color: Colors.darkBlue,
+    },
+  },
+} )
+
+type ButtonProps = {
+  children: ReactNode
+} & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+
+const Button = ( { children, className, ...props }:ButtonProps ) => {
+  const styles = useStyles()
+
+  return (
+    <button type="button" className={cx( styles.btn, className )} {...props}>{children}</button>
+  )
+}
+
+export default Button
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,53 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import { createUseStyles } from 'react-jss'
 
 import Seo from '../components/Seo'
 import Footer from '../components/Footer'
+import { widthLessThan, Breakpoints, Colors } from '../theme'
+import Button from '../components/Button'
 
 const useStyles = createUseStyles( {
   main: {
+  },
+  name: {
+    margin: 0,
+    padding: 0,
+    fontFamily: 'Fugaz One',
+    fontSize: '2.2rem',
+  },
+  links: {
+    marginTop: '0.5rem',
+    display: 'flex',
+    alignContent: 'center',
+    '& > button:not(:first-of-type)': {
+      marginLeft: '1.5rem',
+    },
+  },
+  img: {
+    borderRadius: '9999px',
+    borderColor: `${Colors.yellow} !important`,
+    borderWidth: '0.15rem !important',
+    borderStyle: 'solid !important',
+  },
+  navWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginLeft: '2rem',
+    [ widthLessThan( Breakpoints.tablet ) ]: {
+      marginLeft: 0,
+      marginTop: '0.5rem',
+      alignItems: 'center',
+    },
+  },
+  imgWrapper: {
+    marginTop: '3rem',
+    display: 'flex',
+    [ widthLessThan( Breakpoints.tablet ) ]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
   },
 } )
 
@@ -14,8 +57,18 @@ const Home = () => {
   return (
     <>
       <Seo />
+
       <main className={styles.main}>
-        Home
+        <div className={styles.imgWrapper}>
+          <Image className={styles.img} src="/images/me.png" width={120} height={120} quality={100} priority layout="fixed" />
+          <div className={styles.navWrapper}>
+            <h1 className={styles.name}>Saihajpreet Singh</h1>
+            <nav className={styles.links}>
+              <Link href="/about"><Button>Work</Button></Link>
+              <Link href="/resume"><Button>Resume</Button></Link>
+            </nav>
+          </div>
+        </div>
       </main>
 
       <Footer />
