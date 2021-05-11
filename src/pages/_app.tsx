96d9be672ab3763a160efbf0e5e162b797745082
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-05-10 22:59:55 -0400

    feat: setup react-jss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import 'normalize.css'
+import { jss } from 'react-jss'
 
-const App = ( { Component, pageProps }: AppProps ) => (
-  <Component {...pageProps} />
-)
+import { globalStyles } from '../theme'
 
+jss.createStyleSheet( globalStyles ).attach()
+
+const App = ( { Component, pageProps }: AppProps ) => {
+  useEffect( () => {
+    const style = document.getElementById( 'server-side-styles' )
+
+    if ( style ) {
+      style?.parentNode?.removeChild( style )
+    }
+  }, [] )
+
+  return (
+    <Component {...pageProps} />
+  )
+}
 export default App
