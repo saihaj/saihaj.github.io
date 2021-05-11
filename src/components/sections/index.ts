e8a56207fae2a9b34b6d548015392cd51de66ce0
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-05-10 20:29:53 -0400

    move to NextJS

--- src/components/sections/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MainSection } from './Main'
-export { default as AboutSection } from './About'
-export { default as ProjectSection } from './Project'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,7 @@
-import Seo from '../components/Seo'
-import { AboutSection, MainSection, ProjectSection } from '../components/sections'
-import Container from '../components/Container'
-import Footer from '../components/Footer'
-import Line from '../components/Line'
-
-const IndexPage = () => (
-  <>
-    <Seo title="Home" />
-    <MainSection />
-    <Line />
-    <Container className="pt-16">
-      <AboutSection className="pb-16" />
-    </Container>
-    <Line />
-    <Container className="pt-16">
-      <ProjectSection className="pb-40" />
-    </Container>
-    <Footer />
-  </>
+const Home = () => (
+  <div>
+    Home
+  </div>
 )
 
-export default IndexPage
+export default Home
