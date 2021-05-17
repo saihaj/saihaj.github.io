058805376e2520919038d4411af846c220622128
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-05-17 00:20:09 -0400

    feat: add bottom margin

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,37 @@
 import type { ReactNode, DetailedHTMLProps, HTMLAttributes } from 'react'
+import { createUseStyles } from 'react-jss'
+import cx from 'clsx'
 
 import Seo, { SeoProps } from '../components/Seo'
 import Footer from '../components/Footer'
 import Navbar from './Navbar'
+import { Breakpoints, widthLessThan } from '../theme'
+
+const useStyles = createUseStyles( {
+  main: {
+    [ widthLessThan( Breakpoints.tablet ) ]: {
+      marginBottom: '3rem',
+    },
+    marginBottom: '4rem',
+  },
+} )
 
 export type LayoutProps = {
   children:ReactNode,
 } & DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> & SeoProps
 
-const Page = ( { children, title, description, ...props }:LayoutProps ) => (
-  <>
-    <Seo title={title} description={description} />
-    <Navbar />
-    <main {...props}>
-      {children}
-    </main>
-    <Footer />
-  </>
-)
+const Page = ( { children, title, description, className, ...props }:LayoutProps ) => {
+  const styles = useStyles()
+  return (
+    <>
+      <Seo title={title} description={description} />
+      <Navbar />
+      <main className={cx( styles.main, className )} {...props}>
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
 
 export default Page
