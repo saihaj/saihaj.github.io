b133a8590d2576dc73aeab4609de2b1f4829fd37
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-08-29 13:33:45 -0400

    feat: resume (#54)
    * feat: skills section
    * feat: education, experience and certification section
    * fix: mobile and safari styles

---
+++ src/content/Experience.ts
@@ -0,0 +1,58 @@
+const EXPERIENCE = [
+  {
+    company: 'Giatec Scientific',
+    role: 'Software Developer',
+    description: 'Revolutionizing the concrete industry.',
+    location: 'Ottawa, ON, Canada',
+    startDate: 'May 2021',
+    endDate: '',
+    stack: 'C#, Swift, TypeScript, Python, Azure, Kubernetes, Django, Flask, Angular, Xamarin, Bitrise, GitLabCI',
+  },
+  {
+    company: 'Ambassadors Media',
+    role: 'Co-Founder',
+    description: 'Bring together the best content creators and curate premier content across multiple platforms. Developed website and twitter bot for discord.',
+    location: 'Ottawa, ON, Canada',
+    startDate: 'March 2021',
+    endDate: '',
+    stack: 'TypeScript, Docker, MySQL, Node.js, Gatsby.js, Next.js, TailwindCSS, ReactJSS, Ghost, Strapi, Vercel, Gatsby Cloud, Azure, GitHub Actions',
+  },
+  {
+    company: 'uLytics',
+    role: 'Co-Founder and Engineering',
+    description: 'Developing marketing automation solutions enabled marketers to reach their target audience.',
+    location: 'San Francisco, CA, United States',
+    startDate: 'November 2020',
+    endDate: '',
+    stack: 'TypeScript, Next.js, TailwindCSS, Vercel, GitHub Actions',
+  },
+  {
+    company: 'GraphQL',
+    role: 'Contributor',
+    description: 'Member of graphql-js working group. Migrated JavaScript reference implementation to TypeScript. Helped migrate graphql.org to Gatsby. Help maintain graphql-js, graphql-relay-js, Star Wars API wrapper and express-graphql.',
+    location: 'Open source',
+    startDate: 'June 2020',
+    endDate: '',
+    stack: 'TypeScript, JavaScript, Flow, Node.js, gatsby.js, Netlify, GitHub Actions',
+  },
+  {
+    company: 'ShabadOS',
+    role: 'Software Engineer',
+    description: 'Developing mobile client using React Native. Developing website using Gatsby. Developed web app to create customizable themes for desktop app. Maintainer of documentation site and help maintain various other projects in the organization.',
+    location: 'Open source',
+    startDate: 'April 2020',
+    endDate: '',
+    stack: 'TypeScript, JavaScript, Node.js, React, React Native, Docker, Sentry, SQLite, CircleCI, GitHub Actions, Vercel, Google Cloud, Azure, Nginx',
+  },
+  {
+    company: 'MN Folk Bhangra',
+    role: 'Webmaster',
+    description: 'Developed and managed website using wordpress. Standardized user registration process and hosted a Moodle.',
+    location: 'Plymouth, MN, United States',
+    startDate: 'May 2018',
+    endDate: 'May 2020',
+    stack: 'Wordpress, MoodleCMS, Docker, MySQL, Nginx, Azure',
+  },
+]
+
+export default EXPERIENCE
