fbabc5c348c2b4e6ba337ec9a2c46e741dc38a0b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2023-01-07 22:02:03 -0500

    preload subset loader

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
 const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['normal'] } },
+    ],
+  },
+}
 
 module.exports = withVanillaExtract(nextConfig)
