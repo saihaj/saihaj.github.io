6593bd2ac6f05eb46b1ff0cd551645b680438215
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2024-09-08 21:29:30 -0700

    update deps & run prettier

--- src/content/Experience.ts
+++ src/content/Experience.ts
@@ -15,7 +15,8 @@ const EXPERIENCE = [
     location: 'Everywhere',
     startDate: 'September 2021',
     endDate: '',
-    stack: 'TypeScript, JavaScript, GraphQL, Azure, Kubernetes, Next.js, GitHub Actions, Vercel',
+    stack:
+      'TypeScript, JavaScript, GraphQL, Azure, Kubernetes, Next.js, GitHub Actions, Vercel',
   },
   {
     company: 'Giatec Scientific',
@@ -24,21 +25,25 @@ const EXPERIENCE = [
     location: 'Ottawa, ON, Canada',
     startDate: 'May 2021',
     endDate: '',
-    stack: 'C#, Swift, TypeScript, Python, Azure, Kubernetes, Django, Flask, Angular, Xamarin, Bitrise, GitLabCI',
+    stack:
+      'C#, Swift, TypeScript, Python, Azure, Kubernetes, Django, Flask, Angular, Xamarin, Bitrise, GitLabCI',
   },
   {
     company: 'Ambassadors Media',
     role: 'Co-Founder',
-    description: 'Bring together the best content creators and curate premier content across multiple platforms. Developed website and twitter bot for discord.',
+    description:
+      'Bring together the best content creators and curate premier content across multiple platforms. Developed website and twitter bot for discord.',
     location: 'Ottawa, ON, Canada',
     startDate: 'March 2021',
     endDate: 'September 2021',
-    stack: 'TypeScript, Docker, MySQL, Node.js, Gatsby.js, Next.js, TailwindCSS, ReactJSS, Ghost, Strapi, Vercel, Gatsby Cloud, Azure, GitHub Actions',
+    stack:
+      'TypeScript, Docker, MySQL, Node.js, Gatsby.js, Next.js, TailwindCSS, ReactJSS, Ghost, Strapi, Vercel, Gatsby Cloud, Azure, GitHub Actions',
   },
   {
     company: 'uLytics',
     role: 'Co-Founder and Engineering',
-    description: 'Developing marketing automation solutions enabled marketers to reach their target audience.',
+    description:
+      'Developing marketing automation solutions enabled marketers to reach their target audience.',
     location: 'San Francisco, CA, United States',
     startDate: 'November 2020',
     endDate: '',
@@ -47,25 +52,30 @@ const EXPERIENCE = [
   {
     company: 'GraphQL',
     role: 'Contributor',
-    description: 'Member of graphql-js working group. Migrated JavaScript reference implementation to TypeScript. Helped migrate graphql.org to Gatsby. Help maintain graphql-js, graphql-relay-js, Star Wars API wrapper and express-graphql.',
+    description:
+      'Member of graphql-js working group. Migrated JavaScript reference implementation to TypeScript. Helped migrate graphql.org to Gatsby. Help maintain graphql-js, graphql-relay-js, Star Wars API wrapper and express-graphql.',
     location: 'Open source',
     startDate: 'June 2020',
     endDate: '',
-    stack: 'TypeScript, JavaScript, Flow, Node.js, gatsby.js, Netlify, GitHub Actions',
+    stack:
+      'TypeScript, JavaScript, Flow, Node.js, gatsby.js, Netlify, GitHub Actions',
   },
   {
     company: 'ShabadOS',
     role: 'Software Engineer',
-    description: 'Developing mobile client using React Native. Developing website using Gatsby. Developed web app to create customizable themes for desktop app. Maintainer of documentation site and help maintain various other projects in the organization.',
+    description:
+      'Developing mobile client using React Native. Developing website using Gatsby. Developed web app to create customizable themes for desktop app. Maintainer of documentation site and help maintain various other projects in the organization.',
     location: 'Open source',
     startDate: 'April 2020',
     endDate: '',
-    stack: 'TypeScript, JavaScript, Node.js, React, React Native, Docker, Sentry, SQLite, CircleCI, GitHub Actions, Vercel, Google Cloud, Azure, Nginx',
+    stack:
+      'TypeScript, JavaScript, Node.js, React, React Native, Docker, Sentry, SQLite, CircleCI, GitHub Actions, Vercel, Google Cloud, Azure, Nginx',
   },
   {
     company: 'MN Folk Bhangra',
     role: 'Webmaster',
-    description: 'Developed and managed website using wordpress. Standardized user registration process and hosted a Moodle.',
+    description:
+      'Developed and managed website using wordpress. Standardized user registration process and hosted a Moodle.',
     location: 'Plymouth, MN, United States',
     startDate: 'May 2018',
     endDate: 'May 2020',
